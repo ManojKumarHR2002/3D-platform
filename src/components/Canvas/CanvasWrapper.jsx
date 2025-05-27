2410662753e822287eed587dd25265619c1253cc
Manoj kumar H R <manojkumar123hr@gmail.com>
2025-05-27 15:25:15 +0530

    Resolved and error

--- src/components/Canvas/CanvasWrapper.jsx
+++ src/components/Canvas/CanvasWrapper.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from "react";
 import CanvasArea from "./CanvasArea/CanvasArea";
 import ProjectHierarchyPanel from "./ProjectHierarchyPanel/ProjectHierarchyPanel";
-import { createFileUploader } from "@utils/uploadUtilis";
+import { createFileUploader } from "@utils/UploadUtilis";
 
 
 import TopBar from './TopBar/TopBar';
